e20fcc50ab9f92398dcf69757a4951816db4dc53
Robert Lillyblad <robert.lillyblad@gmail.com>
2022-03-14 15:50:04 -0500

    Initial commit

---
+++ GameController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Mod2Playground
+{
+    class GameController
+    {
+        public static void Run()
+        {
+            bool playing = true;
+            while (playing)
+            {
+                switch (MainMenuSelect())
+                {
+                    case "1":
+                        Console.ResetColor();
+                        Play();
+                        break;
+                    case "2":
+                        Console.ResetColor();
+                        ChangeGameMode();
+                        break;
+                    case "3":
+                        Console.ResetColor();
+                        playing = false;
+                        break;
+                }
+            }
+        }
+        private static string MainMenuSelect()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nMAIN MENU");
+            Console.WriteLine("============");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Change game mode");
+            Console.WriteLine("3. Exit");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("============");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("~ ");
+            return Console.ReadLine();  
+        }
+        private static void ChangeGameMode()
+        {
+            throw new NotImplementedException();
+        }
+        private static void Play()
+        {
+            int guess = -1;
+            Model game = new Model(GetValidInt());
+            while (game.Number != guess)
+            {
+                Console.WriteLine(game.ProcessGuess(guess = GetValidGuess()));
+                game.IncrementGuesses();
+                Console.ResetColor();
+            }
+        }
+
+        private static int GetValidGuess()
+        {
+            bool isGettingInt = true;
+            int gotIt = -1;
+            while (isGettingInt)
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+
+                Console.WriteLine("Please Enter a guess 1-100: ");
+                Console.Write("~ ");
+                int.TryParse(Console.ReadLine(), out gotIt);
+                if (gotIt > 1 && gotIt < 101)
+                    isGettingInt = false;
+            }
+            Console.ResetColor();
+            return gotIt;
+        }
+
+        private static int GetValidInt()
+        {
+            bool isGettingInt = true;
+            int gotIt = -1;
+            while (isGettingInt)
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("Please Enter an upper bound 1-100: ");
+                Console.Write("~ ");
+                int.TryParse(Console.ReadLine(), out gotIt);
+                if (gotIt > 1 && gotIt < 101)
+                    isGettingInt = false;
+            }
+            Console.ResetColor();
+            return gotIt;
+        }
+
+
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2Playground
+{
+    internal class Model
+    {
+        private static int guesses;
+        private static int number;
+
+        public Model(int High)
+        {
+            CreateGuessInRange(High);
+        }
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public static int Guesses
+        {
+            get { return guesses; }
+        }
+        public void IncrementGuesses()
+        {
+            guesses++;
+        }
+        private void CreateGuessInRange(int high)
+        {
+            Random random = new Random();
+            this.Number = random.Next(1, high);
+        }
+        public string ProcessGuess(int Guess)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            string message = "";
+
+            if (Guess > number)
+            {
+                message += "Too high, guess again!";
+            }
+            else if (Guess < number)
+            {
+                message += "Too low, guess again!";
+            }
+            else
+            {
+                message += $"Congratulations, you win! You got it in {guesses} guesses!";
+            }
+            return message;
+        }
+    }
+}
